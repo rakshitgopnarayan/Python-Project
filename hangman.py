d24237806ba90b9489614d88f3d00f7bfa2f9f59
rakshitgopnarayan <63348182+rakshitgopnarayan@users.noreply.github.com>
2020-04-12 18:45:29 +0530

    Add files via upload
    EXTC S.Y.(B.Tech)
    Name                                 ID
    Pranav Bhatane                 181090014
    Rakshit Gopnarayan          181090022
    Pratik Sawale                     181090051
    
    Python is a powerful multi-purpose programming language used by multiple giant companies.
    It has simple and easy to use syntax making it perfect language for someone trying to learn computer programming for first time.
    It is a high-level programming language, and its core design philosophy is all about code readability and a syntax which allows programmers to express concepts in a few lines of code.
    
    In this Project, we will use random module to make a word guessing game.
    This game is for beginners learning to code in python and to give them a little brief about using strings, loops and conditional(If, else) statements.
    
    In this game, there is a list of words present, out of which our interpreter will choose 1 random word.
    The user first has to input their names and then, will be asked to guess any alphabet.
    If the random word contains that alphabet, it will be shown as the output(with correct placement) else the program will ask you to guess another alphabet.
    User will be given 10 turns(can be changed accordingly) to guess the complete word.
    
    This is a Python script of the classic game "Hangman".
    The word to guess is represented by a row of dashes.
    If the player guess a letter which exists in the word, the script writes it in all its correct positions.
    The player has 10 turns to guess the word.
    The word to guess is represented by a row of dashes, giving the number of letters.
    If the guessing player suggests a letter which occurs in the word, the program writes it in all its correct positions.
    If the suggested letter does not occur in the word, the player looses one turn out of total turns(which is actually 10 in our game),
    If player still not able to guess the correct word then The game is over(when player completes total number of turns).
    
    So, it is basically a TWO PLAYER game.But in our project a single player plays the game and the rules are followed by the program.
    
    Hangman is used often by teachers to practice spelling, vocabulary and just for fun.
    The most popular way to play hangman games offline is to draw blank letters for the chosen word on a paper or on the blackboard and let the players guess the letters.
    For each incorrect guess, another part of the man is drawn.
    If the picture is complete before the word is revealed the hangman game is lost and the character is hanged, if the word is revealed before the execution the game is won.

---
+++ hangman.py
@@ -0,0 +1,140 @@
+#importing the time module
+import time
+
+
+#welcoming the user
+name =input(" Enter name of the Player : \n")
+
+print ("Hello, " + name, "Time to play hangman!")
+
+print ("")
+
+#wait for 1 second
+time.sleep(1)
+print("")
+
+print(" _____   ")
+print("|     |  ")
+print("|     O  ")
+print("|    /|\  " )
+print("|    / \  " )
+print("|   ______ ")
+print("|  HANGMAN ")
+
+time.sleep(1.5)
+
+print("Select Difficulty Level : \n")
+print("1. Easy \n")
+print("2. Medium \n")
+print("3. Difficult \n")
+choice=int(input())
+
+if choice==1 :
+#Hint for the word to guess
+    print("Word Hint :- Most commonly used coding language")
+
+    #here we set the word for hint
+    word="python"
+                           
+elif choice==2:
+    print("Word Hint :- Name of the Player who is known as 'God of Cricket' ")
+    #here we set the word for hint
+    word="sachin"
+
+elif choice==3:
+    print("Word Hint :- The first person from India to have 50 million followers on Instagram ")
+    #here we set the word for hint
+    word="viratkohli"
+
+time.sleep(2.0)
+print("")
+print("So Here you Go.....!!!!")
+
+
+print ("Start guessing...")
+time.sleep(0.5)
+
+               
+
+#creates an variable with an empty value
+guesses = ''
+
+#determine the number of turns
+turns =len(word)
+
+# Create a while loop
+
+#check if the turns are more than zero
+while turns > 0:         
+
+    # make a counter that starts with zero
+    failed = 0             
+
+    # for every character in secret_word    
+    for char in word:      
+
+    # see if the character is in the players guess
+        if char in guesses:    
+    
+        # print then out the character
+            print (char,end=' '),    
+
+        else:
+    
+        # if not found, print a dash
+            print ("_",end=' '),     
+       
+        # and increase the failed counter with one
+            failed += 1    
+
+    # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:
+        time.sleep(1)
+        print('')
+        
+        print ("You won")
+
+        print(" _____   ")
+        print("|     |  ")
+        print("|        ")
+        print("|     O  ")
+        print("|    /|\ ")
+        print("|____/_\______  " )
+        print("  YOU SAVED ME "+name)
+
+    # exit the script
+        break              
+    print('')
+
+    # ask the user go guess a character
+    guess =input("guess a character: ") 
+
+    # set the players guess to guesses
+    guesses += guess                    
+
+    # if the guess is not found in the secret word
+    if guess not in word:  
+ 
+     # turns counter decreases with 1 (now 9)
+        turns -= 1        
+ 
+    # print wrong
+        print ("Oops..!! Wrong")    
+ 
+    # how many turns are left
+        print ("You have", + turns, 'more guesses') 
+ 
+    # if the turns are equal to zero
+        if turns == 0:           
+    
+        # print "You Lose" 
+            
+            print(" ______   ")
+            print("|      |  ")
+            print("|      O  ")
+            print("|    /|\  " )
+            print("|    | |  " )
+            print("|__________ ")
+            print ("You Killed me "+name,"\n Looser !!!!" ) 
